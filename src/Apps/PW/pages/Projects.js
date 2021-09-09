1ad48d3d76a64e97c18b4b0364faa9ae1f4da65f
Jeffrey Carr <jeff@Jeffs-Computer.local>
2021-09-09 14:13:03 -0400

    Connected page to Github API to pull public projects from my repo

--- src/Apps/PW/pages/Projects.js
+++ src/Apps/PW/pages/Projects.js
@@ -1,13 +1,49 @@
-import { Grommet } from 'grommet';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import Axios from 'axios';
+import { Box, Button, Grid, Heading } from 'grommet';
+import { LinkPrevious } from 'grommet-icons';
+
 import AppBar from '../../../globals/components/AppBar';
+import ProjectCard from '../components/ProjectCard';
 
 function Projects() {
+    const [repoList, setRepoList] = useState([]);
+
+    // Get projects list
+    useEffect(() => {
+        Axios.get('https://api.github.com/users/jcarr98/repos').then((data) => {
+            setRepoList(data.data);
+        });
+    }, []);
+
     return (
-        <div>
+        <Box>
             <AppBar />
-            <h1>Projects Page</h1>
-        </div>
+
+            {/* Home link */}
+            <Box alignSelf="start" pad="medium">
+                <Button 
+                    href="http://localhost:3000/" 
+                    color="main" 
+                    icon={ <LinkPrevious color="main" size="medium" /> } 
+                    label="Back to Home"
+                    plain
+                />
+            </Box>
+
+            <Box width="full" align="center">
+                <Heading>My Projects</Heading>
+            </Box>
+            
+            <Grid gap="large" pad="medium" columns={{ count: 'fit', size: "medium"}}>
+                {repoList.map((val,key) => {
+                    return(
+                        <ProjectCard key={val.id} name={val.name} description={val.description} url={val.html_url} />
+                    );
+                })}
+            </Grid>
+        </Box>
     );
 }
 

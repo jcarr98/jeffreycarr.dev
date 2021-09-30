0947002f53f7cce6f3f751984892e95c232d1532
Jeffrey Carr <jeff@Jeffs-Computer.local>
2021-09-30 09:26:32 -0400

    Changed name from 'Menu' to 'RecipeMenu' in order to use Grommet's Menu component - didn't end up using it anyways

--- src/Apps/RecipeBook/RecipeRouter.js
+++ src/Apps/RecipeBook/RecipeRouter.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Menu from './pages/Menu';
+import { CookiesProvider } from 'react-cookie';
+
+import RecipeMenu from './pages/RecipeMenu';
 import Recipe from './pages/Recipe';
 import Oops from '../../globals/pages/Oops';
 
 function RecipeRouter() {
     return(
-        <Router>
-            <Switch>
-                <Route path="/" exact component={Menu} />
-                <Route path="/recipe/:id" component={Recipe} />
-                <Route component={Oops} />
-            </Switch>
-        </Router>
+        <CookiesProvider>
+            <Router>
+                <Switch>
+                    <Route path="/" exact component={RecipeMenu} />
+                    <Route path="/recipe/:id" component={Recipe} />
+                    <Route component={Oops} />
+                </Switch>
+            </Router>
+        </CookiesProvider>
     );
 }
 
